8c69e2c3bf0524cdb416916fa4fd6f2dc1b661a8
boberf <fabio.oberhofer@bbcag.ch>
2023-01-19 11:23:05 +0100

    src: added basic addition

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,31 @@
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello World" << std::endl;
+    int first = 0;
+    int func  = 0;
+    int second = 0;
+    std::cout << "Enter first number: ";
+    std::cin >> first;
+    std::cout << "Choose between:\n1)Addition\n2)Subtraction\n3)Multiplication\n4)Division" << std::endl;
+    std::cin >> func;
+    std::cout << "Enter second number: ";
+    std::cin >> second;
+    switch (func)
+    {
+    case 1:
+        std::cout << first + second << std::endl;
+        break;
+    case 2:
+        /* code */
+        break;
+    case 3:
+        /* code */
+        break;
+    case 4:
+        /* code */
+        break;
+    default:
+        break;
+    }
     return 0;
 }
